ed832744e6dacbea137a98622f2160d4d0a9b50c
Joel VG <69538363+joelx1@users.noreply.github.com>
2025-12-14 20:26:42 +0000

    structure upload
    made structure

---
+++ src/main/App.java
@@ -0,0 +1,9 @@
+package com.jx1.libsearch;
+public class App 
+{
+    public static void main( String args[])
+    {
+        System.out.println( "Hello World!" );
+        System.out.println( "This is library-search" );
+    }
+}
